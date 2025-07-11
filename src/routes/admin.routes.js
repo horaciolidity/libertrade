b844c39de8394a9d57038d1f163ed2b86c63769f
horaciolidity <horacio.dev.sol@gmail.com>
2025-07-10 21:32:20 -0300

    Primer commit

--- src/routes/admin.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getUsers, depositToUser } = require('../controllers/admin.controller');
-
-router.get('/users', getUsers);
-router.post('/deposit', depositToUser);
-
-module.exports = router;
